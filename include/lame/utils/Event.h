620ad5e09d47b08ffff56fea15a0aaab90d4259f
polame <polame.dev@gmail.com>
2025-10-26 22:42:05 +0300

    Добавил начальный проект и Event

---
+++ include/lame/utils/Event.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+typedef bool LAME_Event;
+
+/**
+ * @brief  Инициализация события
+ * @param  event: дескриптор события
+ */
+void LAME_Event_Init(LAME_Event *event);
+
+/**
+ * @brief  Получить событие, событие сбросится, если оно произошло
+ * @param  event: дескриптор события
+ * @return true - событие произошло
+ * @return false - событие не произошло
+ */
+bool LAME_Event_Take(LAME_Event *event);
+
+/**
+ * @brief  Проверить событие
+ * @param  event: дескриптор события
+ * @return true - событие произошло
+ * @return false - событие не произошло
+ */
+bool LAME_Event_Check(const LAME_Event *event);
+
+/**
+ * @brief  Установить событие
+ * @param  event: дескриптор события
+ */
+void LAME_Event_Set(LAME_Event *event);
+
+/**
+ * @brief  Сбросить событие
+ * @param  event: дескриптор события
+ */
+void LAME_Event_Clear(LAME_Event *event);
+
+#ifdef __cplusplus
+}
+#endif
